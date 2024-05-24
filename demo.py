c862ac73f6c297115571d69331e57953aeaf05f7
Gorkor <101441645+Gorkor227@users.noreply.github.com>
2024-05-25 06:54:58 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
 from awsPrint import print, FontColor, sepLine
+
 li = {
     'today': 'Mon',
     'time_hour': '15',
@@ -14,3 +15,24 @@ print(li, color=FontColor.CYAN)
 # [sepLine default] string:'——', multiple:50, color:FontColor.YELLOW
 sepLine(color=FontColor.WHITE)
 print(li, color=FontColor.cyan, isFormat=True)
+
+
+def show_all_color():
+    colorLower = []
+    colorUpper = []
+    for font_key, font_value in FontColor.__dict__.items():
+        font_key: str
+        if not font_key.startswith('__'):
+            if font_key.islower() and font_key != 'reset':
+                colorLower.append((font_key, font_value))
+            elif font_key.isupper():
+                colorUpper.append((font_key, font_value))
+
+    print(colorLower, isFormat=True)(colorUpper, isFormat=True)
+    sepLine(color=FontColor.WHITE)
+    for lower, upper in zip(colorLower, colorUpper):
+        print(lower[0], color=lower[1], end=' ')(upper[0], color=upper[1])
+    print('reset', color=FontColor.reset)
+
+
+show_all_color()
