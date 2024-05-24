083ef9d41b15f9c9416e1c3e6ef10eee35831812
Gorkor <101441645+Gorkor227@users.noreply.github.com>
2024-05-25 04:39:50 +0800

    demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,16 @@
-# !/usr/bin/env python
-# -*- coding: utf-8 -*-
-# ------------------------------
-""""""
-from color_print import print
-print('hello')
-
+from color_print import print, FontColor, sepLine
+li = {
+    'today': 'Mon',
+    'time_hour': '15',
+    'time_minute': '40',
+    'time_second': '20',
+    'a': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+}
+# [print default]: color:'FontColor.YELLOW' isFormat(use pprint.pformat):False
+print('Hi CPrint')
+print('Hello CPrint', color=FontColor.RED)
+print('Hello', color=FontColor.GREEN, end=' ')('world')
+print(li, color=FontColor.CYAN)
+# [sepLine default] string:'——', multiple:50, color:FontColor.YELLOW
+sepLine(color=FontColor.WHITE)
+print(li, color=FontColor.cyan, isFormat=True)
